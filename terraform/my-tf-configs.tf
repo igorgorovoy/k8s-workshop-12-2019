2343269044f871721aebffaae3051c3aa1dca1ac
Igor Gorovoy <igorgorovoy@ukr.net>
2019-12-08 14:28:05 +0200

    terraform provision

---
+++ terraform/my-tf-configs.tf
@@ -0,0 +1,19 @@
+provider "kubernetes" {}
+
+resource "kubernetes_pod" "test" {
+  metadata {
+    name = "terraform-example"
+  }
+
+  spec {
+    container {
+      image = "nginx:1.7.9"
+      name  = "example"
+
+      env {
+        name  = "environment"
+        value = "test"
+      }
+    }
+  }
+}
